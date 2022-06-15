d3f6fbfe5ac330cc07594c1fb13474753b1ac505
RealAgnetha <carolinatrack@googlemail.com>
2022-06-15 19:18:47 +0200

    KBE-47 send message

--- src/main/java/com/example/microservice/service/rabbit/Receiver.java
+++ src/main/java/com/example/microservice/service/rabbit/Receiver.java
@@ -1,12 +1,20 @@
 package com.example.microservice.service.rabbit;
 
+import java.util.concurrent.CountDownLatch;
 import org.springframework.stereotype.Component;
 
-//POJO that defines a method for receiving messages
 @Component
 public class Receiver {
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
+        latch.countDown();
     }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
 }

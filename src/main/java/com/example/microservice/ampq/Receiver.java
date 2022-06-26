d8521e89dfe4f53c0a92f39ad97c946321f8a7eb
RealAgnetha <carolinatrack@googlemail.com>
2022-06-26 19:02:05 +0200

    adjust price service

--- src/main/java/com/example/microservice/ampq/Receiver.java
+++ src/main/java/com/example/microservice/ampq/Receiver.java
@@ -3,13 +3,15 @@ package com.example.microservice.ampq;
 import com.example.microservice.service.PriceService;
 import com.example.microservice.testDTOs.PriceMSRequest;
 import com.example.microservice.testDTOs.PriceMSResponse;
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.logging.Logger;
 
+@Slf4j
 @RabbitListener(id = "price_listener", queues = "calculate-price", returnExceptions = "true")
 public class Receiver {
 
